6a0f65563c011aa411913ca126e9d7672775b5a3
Sigitas <s.grincevicius@email.com>
2026-04-19 14:56:45 +0300

    Added duplicate email test

--- src/users/auth/auth.controller.ts
+++ src/users/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, ClassSerializerInterceptor, Controller, Post, SerializeOptions, UseInterceptors } from '@nestjs/common';
 import { CreateUserDto } from '../create-user.dto';
 import { AuthService } from './auth.service';
 import { User } from '../user.entity';
@@ -6,12 +6,15 @@ import { LoginDto } from '../user/login.dto';
 import { LoginResponse } from '../login.response';
 
 @Controller('auth')
+@UseInterceptors(ClassSerializerInterceptor)
+@SerializeOptions({strategy: 'excludeAll'})
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto): Promise<User> {
-    return await this.authService.register(createUserDto);
+    const user = await this.authService.register(createUserDto);
+    return user;
   }
 
   @Post('login')
